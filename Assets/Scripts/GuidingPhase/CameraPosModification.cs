4057d1cf53fb6373d9d16aec8be907a7d8f7e6ec
DESKTOP-0U8FMHA\tsumiki_02 <dddd@gmail.com>
2018-05-22 17:30:27 +0900

    Add a component to modify camera position when the player is hit by a truck

---
+++ Assets/Scripts/GuidingPhase/CameraPosModification.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraPosModification : MonoBehaviour {
+
+    public GameObject cameraRig;
+    public GameObject viveCamera;
+    public bool isActivated = false;
+
+    public void InterpolateCamPos() {
+
+    }
+}
