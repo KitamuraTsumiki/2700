327c99d069afb136e4816b41794a40a48fd7b54c
Takashi-Odagiri <takashi.odagiri@tsumikiseisaku.com>
2018-06-05 16:51:30 +0900

    Add event system for phase management

---
+++ Assets/Scripts/ReplayPhase/ReplayContentManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// This class manages choreography of the "replay" phase
+/// </summary>
+public class ReplayContentManager : ContentManager
+{
+
+	private void Start () {
+		
+	}
+
+    public override void Pause()
+    {
+
+    }
+
+    private void Update () {
+        Debug.Log("you're in the replay phase");
+    }
+}
